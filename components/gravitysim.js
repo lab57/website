cf9a05493b1fd5113faeebf8331c2de8c93f2be6
Luc Barrett <luc.barrett57@gmail.com>
2023-07-15 00:24:59 -0400

    Somewhat functioning?

--- components/gravitysim.js
+++ components/gravitysim.js
@@ -1,6 +1,5 @@
 import React from "react";
 import dynamic from 'next/dynamic'
-
 // Will only import `react-p5` on client-side
 const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
     ssr: false,
@@ -29,8 +28,13 @@ class particle {
         return Math.sqrt((p2.x - this.x) ** 2 + (p2.y - this.y) ** 2)
     }
 
-    getWallForce() {
-        return
+    applyWallForce(p5) {
+        //handle x
+
+    }
+    applyVelocities() {
+        this.x += this.vx
+        this.y += this.vy
     }
 }
 
@@ -38,6 +42,7 @@ class particle {
 class myComp extends React.Component {
     p1;
     p2;
+    p3;
     particles;
     tstep;
     constructor(props) {
@@ -49,19 +54,26 @@ class myComp extends React.Component {
         // use parent to render the canvas in this ref
         // (without that p5 will render the canvas outside of your component)
         p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
-        this.p1 = new particle(0, 0, 0)
-        this.p2 = new particle(1, p5.windowWidth, p5.windowHeight)
-        this.particles = [this.p1, this.p2]
+        this.p1 = new particle(1, 1, 1)
+        this.p2 = new particle(1, p5.windowWidth - 1, p5.windowHeight - 1)
+        this.p3 = new particle(1, 5, p5.windowHeight - 1)
+
+        this.particles = []
+        for (let i = 0; i < 5; i++) {
+            this.particles.push(new particle(i, Math.random() * p5.windowWidth, Math.random() * p5.windowHeight))
+        }
+
+
     }
 
     draw = (p5) => {
 
-        this.takeStep()
+        this.takeStep(p5)
 
         p5.clear()
-        p5.ellipse(this.p1.x, this.p1.y, 15, 15);
-        //p5.ellipse(p5.windowWidth / 2, p5.windowHeight / 2, 15, 15);
-        p5.ellipse(this.p2.x, this.p2.y, 15, 15);
+        for (particle of this.particles) {
+            p5.ellipse(particle.x, particle.y, 15, 15);
+        }
         p5.noStroke()
 
         // NOTE: Do not use setState in the draw function or in functions that are executed
@@ -69,7 +81,7 @@ class myComp extends React.Component {
         // please use normal variables or class properties for these purposes
     };
 
-    takeStep = () => {
+    takeStep = (p5) => {
         for (let p of this.particles) {
             for (let p_2 of this.particles) {
                 if (p != p_2) {
@@ -79,8 +91,9 @@ class myComp extends React.Component {
                     let theta = Math.atan2(ydif, xdif)
                     p.vx += F * Math.cos(theta)
                     p.vy += F * Math.sin(theta)
-                    p.x += p.vx
-                    p.y += p.vy
+                    p.applyWallForce(p5)
+                    p.applyVelocities()
+
                 }
             }
         }
